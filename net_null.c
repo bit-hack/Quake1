94b11e657fdf5af22ae805c5658101c18af9a62b
Aidan Dodds <aidan@codeplay.com>
2018-03-27 10:31:00 +0100

    Add net null skeleton

---
+++ net_null.c
@@ -0,0 +1,36 @@
+#include "quakedef.h"
+
+static int NetNull_Init(void) {}
+static void NetNull_Listen(bool state) {}
+static void NetNull_SearchForHosts(bool xmit) {}
+static qsocket_t* NetNull_Connect(char* host) {}
+static qsocket_t* NetNull_CheckNewConnections(void) {}
+static int NetNull_QGetMessage(qsocket_t* sock) {}
+static int NetNull_QSendMessage(qsocket_t* sock, sizebuf_t* data) {}
+static int NetNull_SendUnreliableMessage(qsocket_t* sock, sizebuf_t* data) {}
+static bool NetNull_CanSendMessage(qsocket_t* sock) {}
+static bool NetNull_CanSendUnreliableMessage(qsocket_t* sock) {}
+static void NetNull_Close(qsocket_t* sock) {}
+static void NetNull_Shutdown(void) {}
+
+static net_driver_t NetNullDriver = {
+    "NetNullDriver",
+    true,
+    NetNull_Init,
+    NetNull_Listen,
+    NetNull_SearchForHosts,
+    NetNull_Connect,
+    NetNull_CheckNewConnections,
+    NetNull_QGetMessage,
+    NetNull_QSendMessage,
+    NetNull_SendUnreliableMessage,
+    NetNull_CanSendMessage,
+    NetNull_CanSendUnreliableMessage,
+    NetNull_Close,
+    NetNull_Shutdown,
+    0,
+};
+
+net_driver_t *getNetNullDriver() {
+  return &NetNullDriver;
+}
